d36cd03de834785be3b12b9f0a219832bb66301c
Msiperko <matthewsiperko@gmail.com>
2020-03-13 14:23:29 -0400

    updated auth logic

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,7 +7,7 @@ const authEvents = require('./auth/events')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
-
+// created user auth 
 $(() => {
   $('#sign-up').on('submit', authEvents.onSignUp)
   $('#sign-in').on('submit', authEvents.onSignIn)
